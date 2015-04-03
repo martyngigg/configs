c221cda43a21f253240f2e576e20f851654f355e
Martyn Gigg <martyn.gigg@gmail.com>
2015-04-03 23:15:42 +0100

    Start putting together scripts for installation

---
+++ setup/packages/README.md
@@ -0,0 +1,2 @@
+Simple flat files containing one package per line to be installed by
+the setup script.
---
+++ setup/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+################################################################################
+# setup.sh: Assuming a clean system: setup standard packages and install
+#           config scripts stored here
+#
+#   curl -Ls https://github.com/martyngigg/configs/raw/master/setup/setup.sh | bash
+#
+# Author: Martyn Gigg
+################################################################################
