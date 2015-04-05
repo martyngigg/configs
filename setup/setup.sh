3ef88ab288357bdd4a5fc9e59dd2eb3e54cf1b95
Martyn Gigg <martyn.gigg@gmail.com>
2015-04-05 10:58:00 +0100

    Use inconsolata as terminal & emacs font
    Also set other terminal fonts from setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -134,7 +134,15 @@ else
 fi
 
 # ------------------------------------------------------------------------------
-# solarize gnome theme
+# customize gnome-terminal theme
 # ------------------------------------------------------------------------------
 info "installing solarized theme for gnome terminal"
 $local_clone_dir/setup/gnome-terminal-colors-solarized/install.sh --scheme=dark --profile=Default
+info "setting terminal defaults"
+gconftool-2 -s /apps/gnome-terminal/profiles/Default/font --type string "Inconsolata Medium 12"
+gconftool-2 -s /apps/gnome-terminal/profiles/Default/use_system_font --type boolean false
+gconftool-2 -s /apps/gnome-terminal/profiles/Default/use_custom_default_size --type boolean false
+gconftool-2 -s /apps/gnome-terminal/profiles/Default/default_size_rows --type int 49
+gconftool-2 -s /apps/gnome-terminal/profiles/Default/default_size_columns --type int 102
+gconftool-2 -s /apps/gnome-terminal/profiles/Default/silent_bell --type boolean true
+gconftool-2 -s /apps/gnome-terminal/profiles/Default/default_show_menubar --type boolean false
