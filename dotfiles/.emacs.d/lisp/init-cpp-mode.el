5188665edbf0fd0e1009d3958db6a7f4489f7ced
Martyn Gigg <martyn.gigg@gmail.com>
2017-03-15 22:42:00 +0000

    Configure Irony, Company & Flycheck

--- dotfiles/.emacs.d/lisp/init-cpp-mode.el
+++ dotfiles/.emacs.d/lisp/init-cpp-mode.el
@@ -18,19 +18,54 @@
 ;;----------------------------------------------------------------------------
 ;; Irony - source code completion
 ;;----------------------------------------------------------------------------
-;; (require-package 'irony)
-;; (add-hook 'c++-mode-hook 'irony-mode)
-;; (add-hook 'c-mode-hook 'irony-mode)
-;; (add-hook 'objc-mode-hook 'irony-mode)
+(require-package 'irony)
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
 
-;; (defun my-irony-mode-hook ()
-;;   (define-key irony-mode-map [remap completion-at-point]
-;;     'irony-completion-at-point-async)
-;;   (define-key irony-mode-map [remap complete-symbol]
-;;     'irony-completion-at-point-async))
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
 
-;; (add-hook 'irony-mode-hook 'my-irony-mode-hook)
-;; (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+;;----------------------------------------------------------------------------
+;; Company - in-buffer completion
+;;----------------------------------------------------------------------------
+(require-package 'company-irony)
+(require-package 'company-irony-c-headers)
+
+(add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
+;;(setq company-backends (delete 'company-semantic company-backends))
+(eval-after-load 'company
+  '(add-to-list
+    'company-backends '(company-irony-c-headers company-irony)))
+
+(setq company-idle-delay 0)
+(define-key c-mode-map [(tab)] 'company-complete)
+(define-key c++-mode-map [(tab)] 'company-complete)
+
+;;----------------------------------------------------------------------------
+;; Flycheck - syntax checking
+;;----------------------------------------------------------------------------
+(require-package 'flycheck)
+(add-hook 'c++-mode-hook 'flycheck-mode)
+(add-hook 'c-mode-hook 'flycheck-mode)
+
+(require 'flycheck-rtags)
+(defun my-flycheck-rtags-setup ()
+  (flycheck-select-checker 'rtags)
+  (setq-local flycheck-highlighting-mode nil) ;; RTags creates more accurate overlays.
+  (setq-local flycheck-check-syntax-automatically nil))
+;; c-mode-common-hook is also called by c++-mode
+(add-hook 'c-mode-common-hook #'my-flycheck-rtags-setup)
+
+(require-package 'flycheck-irony)
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
 
 
 (provide 'init-cpp-mode)
