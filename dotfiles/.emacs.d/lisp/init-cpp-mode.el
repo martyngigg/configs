535633e66cdce7e79766ce43dff09664e5ea7694
Martyn Gigg <martyn.gigg@gmail.com>
2017-03-19 13:20:13 +0000

    Simplify C++-mode registration

--- dotfiles/.emacs.d/lisp/init-cpp-mode.el
+++ dotfiles/.emacs.d/lisp/init-cpp-mode.el
@@ -1,11 +1,8 @@
+;;----------------------------------------------------------------------------
 ;; File associations
-(setq auto-mode-alist (cons '("\\.C$" . c++-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.cc$" . c++-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.c$" . c++-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.h$" . c++-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.cpp$" . c++-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.cxx$" . c++-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.tcc$" . c++-mode) auto-mode-alist))
+;;----------------------------------------------------------------------------
+(add-auto-mode 'c++-mode "\\.C$" "\\.cc$"
+               "\\.c$" "\\.h$" "\\.cpp$" "\\.cxx$" "\\.tcc$")
 
 ;; ;;----------------------------------------------------------------------------
 ;; ;; RTags - source code navigation
--- dotfiles/.emacs.d/lisp/init-utils.el
+++ dotfiles/.emacs.d/lisp/init-utils.el
@@ -17,20 +17,6 @@
 
 
 ;;----------------------------------------------------------------------------
-;; String utilities missing from core emacs
-;;----------------------------------------------------------------------------
-(defun sanityinc/string-all-matches (regex str &optional group)
-  "Find all matches for `REGEX' within `STR', returning the full match string or group `GROUP'."
-  (let ((result nil)
-        (pos 0)
-        (group (or group 0)))
-    (while (string-match regex str pos)
-      (push (match-string group str) result)
-      (setq pos (match-end group)))
-    result))
-
-
-;;----------------------------------------------------------------------------
 ;; Delete the current file
 ;;----------------------------------------------------------------------------
 (defun delete-this-file ()
