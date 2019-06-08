fe6bac8431ffe63b817b161821fa602dc8d29b23
Martyn Gigg <martyn.gigg@gmail.com>
2019-06-08 12:18:09 +0100

    Add docker-ce to basic set of packages to install

--- setup/install-packages-apt.sh
+++ setup/install-packages-apt.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+setup_dir=$(cd $(dirname "$0") && pwd)
+. $setup_dir/common.sh
+
+info updating package lists
+sudo apt-get -y update
+
+info upgrading current packages
+sudo apt-get -y upgrade
+
+info removing old packages
+sudo apt-get -y autoremove
+
+info installing base packages
 sudo apt-get -y install \
   build-essential \
   cmake \
@@ -26,3 +39,26 @@ sudo apt-get -y install \
   silversearcher-ag \
   xsel \
   zsh
+
+info adding docker-ce repo
+# allow apt to use a repository over HTTPS:
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# At time of writing there is no stable package for Ubuntu 19.04
+info add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   test"
+sudo apt-get -y update
+
+info installing docker
+sudo apt-get -y install \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io \
