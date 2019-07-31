007dac3be9422882c11d7a1f152281f8ec37446f
Martyn Gigg <martyn.gigg@gmail.com>
2019-07-31 10:44:15 +0100

    Update config for whole-line-or-region mode

--- dotfiles/.emacs.d/lisp/init-editing.el
+++ dotfiles/.emacs.d/lisp/init-editing.el
@@ -42,7 +42,6 @@
  ring-bell-function 'sanityinc/flash-mode-line)
 
 
-
 ;; Newline behaviour
 
 ;; None of this fancy indenting nonsense
@@ -190,9 +189,9 @@
 ;; Cut/copy the current line if no region is active
 ;;----------------------------------------------------------------------------
 (require-package 'whole-line-or-region)
-(whole-line-or-region-mode t)
-(diminish 'whole-line-or-region-mode)
-(make-variable-buffer-local 'whole-line-or-region-mode)
+(whole-line-or-region-local-mode t)
+(diminish 'whole-line-or-region-local-mode)
+(make-variable-buffer-local 'whole-line-or-region-local-mode)
 
 (defun suspend-mode-during-cua-rect-selection (mode-name)
   "Add an advice to suspend `MODE-NAME' while selecting a CUA rectangle."
